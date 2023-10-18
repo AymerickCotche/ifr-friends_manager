287ac146fa46cb7eb403d71d473d3531a8c2e7ce
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-10-18 09:51:22 +0400

    prisma ok

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+import prisma from '@/../lib/prisma';
+
+async function getProfiles(id: number) {
+  const profile = await prisma.profile.findUnique({
+    where: {
+      id: id,
+    },
+   
+  });
+  return {
+    props: profile,
+  };
+ 
+
+}
+
+export default async function Home() {
+
+  const profile = await getProfiles(2)
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>{profile.props?.firstName}</p>
+    </main>
+  )
+}
